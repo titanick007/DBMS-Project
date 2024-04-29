ad9edfa6269a0f28f8d4bb78f52cc5818a144ad0
Samuel E George <142198315+titanick007@users.noreply.github.com>
2024-04-29 16:48:18 +0530

    database changes

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # DBMS PROJECT
 
+Welcome to the E-Store Management System project! This project aims to provide a database management system for an online store.
 
+## Getting Started
+
+To set up the database for the E-Store Management System, follow these steps:
+
+
+
+3. Run the SQL file named `estore.sql` to create the necessary tables and insert sample data into the database. Make sure you have a MySQL server running and replace `<database-name>` with the name of your database:
+
+4. After running the SQL file, the database schema and sample data will be set up in your MySQL database.
+
+## Database Connection
+
+The database connection for the E-Store Management System is configured in the `db.js` file located in the `models` folder. Make sure to update the database connection details in this file to match your MySQL database configuration:
--- estore.sql
+++ estore.sql
@@ -1,54 +1,26 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 18, 2018 at 10:18 AM
--- Server version: 10.1.24-MariaDB
--- PHP Version: 7.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE admin (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    username VARCHAR(128) NOT NULL UNIQUE,
+    email VARCHAR(128) NOT NULL UNIQUE,
+    phone VARCHAR(128) NOT NULL,
+    password VARCHAR(128) NOT NULL,
+    gender VARCHAR(128) NOT NULL,
+    dob DATE NOT NULL,
+    nid VARCHAR(256) NOT NULL,
+    presentaddress VARCHAR(256) NOT NULL,
+    parmanentaddress VARCHAR(256) NOT NULL
+);
 
---
--- Database: `estore`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `username` varchar(128) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `phone` varchar(128) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `gender` varchar(128) NOT NULL,
-  `dob` date NOT NULL,
-  `nid` varchar(256) NOT NULL,
-  `presentaddress` varchar(256) NOT NULL,
-  `parmanentaddress` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `name`, `username`, `email`, `phone`, `password`, `gender`, `dob`, `nid`, `presentaddress`, `parmanentaddress`) VALUES
-(1, 'Noyon', 'shanto', 'shanto@gmail.com', '', 'sha1$ebb2bdc9$1$d77148ab8c6268c2f43b5fafdfa3f0405bd24306', 'male', '1996-05-24', '19979972342363842', 'H-23,R-4,Nikunja-2,Khilkhet,Dhaka', 'Dhaka'),
-(2, 'arnob', 'arnob', 'ar@gmIL.COM', '', 'sha1$f77072c1$1$ffafbbe013a168ccde2c4c3ee99cc322e05854c6', 'male', '9843-12-12', '3245465756543', 'wgbnb,hstr', 'geawaiuobvgair,gaer');
+INSERT INTO admin (name, username, email, phone, password, gender, dob, nid, presentaddress, parmanentaddress) 
+VALUES ('Samuel E George', 'samuel', 'samuel@gmail.com', '7873928873', 'password123', 'Male', '1990-01-01', '1234567890123', '123 Main St', '456 Elm St');
 
 -- --------------------------------------------------------
 
@@ -56,28 +28,33 @@ INSERT INTO `admin` (`id`, `name`, `username`, `email`, `phone`, `password`, `ge
 -- Table structure for table `product`
 --
 
-CREATE TABLE `product` (
-  `id` int(11) NOT NULL,
-  `productname` varchar(256) NOT NULL,
-  `price` int(8) NOT NULL,
-  `quantity` int(8) NOT NULL,
-  `catagory` varchar(64) NOT NULL,
-  `image1` varchar(256) DEFAULT NULL,
-  `image2` varchar(256) DEFAULT NULL,
-  `image3` varchar(256) DEFAULT NULL,
-  `details` varchar(8192) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE product (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    productname VARCHAR(256) NOT NULL,
+    price INT NOT NULL,
+    quantity INT NOT NULL,
+    catagory VARCHAR(64) NOT NULL,
+    image1 VARCHAR(256),
+    details VARCHAR(8192) NOT NULL
+);
 
---
--- Dumping data for table `product`
---
 
-INSERT INTO `product` (`id`, `productname`, `price`, `quantity`, `catagory`, `image1`, `image2`, `image3`, `details`, `date`) VALUES
-(2, 'Sony HD 4K Smart LED TV', 350, 1, 'Electronics', 'https://www.suryaelectronics.in/CommonImageHandler/ImageHandler.ashx?imagepath=~/img/Product/Main/SONYHomeEntertainmentTelevision157.jpg&width=1080', NULL, NULL, '', CURDATE()),
-(3, 'Marie Gold Biscuits', 12, 200, 'Food', 'https://www.bigbasket.com/media/uploads/p/l/264572-2_5-britannia-marie-gold-biscuits.jpg', NULL, NULL, 'Lohar Biskut', CURDATE()),
-(6, 'Amazfit Bip Smart Watch', 4800, 1, 'Electronics', 'https://in.amazfit.com/cdn/shop/products/2b88b81321c7510919fdfdbb9c69360e_2048x.jpg?v=1691659249', NULL, NULL, 'Watch', CURDATE()),
-(10, 'iPhone 13', 1200, 10, 'Electronics', 'https://media.croma.com/image/upload/v1664009491/Croma%20Assets/Communication/Mobiles/Images/243460_0_n90peu.png', NULL, NULL, '', CURDATE());
+
+
+INSERT INTO product (productname, price, quantity, catagory, image1, details) 
+VALUES ('Sony HD 4K Smart LED TV', 35000, 4, 'Electronics', 'https://www.suryaelectronics.in/CommonImageHandler/ImageHandler.ashx?imagepath=~/img/Product/Main/SONYHomeEntertainmentTelevision157.jpg&width=1080', '');
+
+INSERT INTO product (productname, price, quantity, catagory, image1, details) 
+VALUES ('Marie Gold Biscuits', 12, 190, 'Food', 'https://www.bigbasket.com/media/uploads/p/l/264572-2_5-britannia-marie-gold-biscuits.jpg', '');
+
+INSERT INTO product (productname, price, quantity, catagory, image1, details) 
+VALUES ('Amazfit Bip Smart Watch', 4800, 5, 'Electronics', 'https://in.amazfit.com/cdn/shop/products/2b88b81321c7510919fdfdbb9c69360e_2048x.jpg?v=1691659249', 'Watch');
+
+INSERT INTO product (productname, price, quantity, catagory, image1, details) 
+VALUES ('iPhone 13', 1200, 16, 'Electronics', 'https://media.croma.com/image/upload/v1664009491/Croma%20Assets/Communication/Mobiles/Images/243460_0_n90peu.png', '');
+
+INSERT INTO product (productname, price, quantity, catagory, image1, details) 
+VALUES ('iphone 14', 79000, 47, 'Electronics', 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTwHGFf3u2gQQ3qnRoLaEQfAedADIHQfcFvioVHjXhpH4VINK0AFVjtZvEEVbLTdoLG1mo1oD0kFhn3nuwHd01MpbAz4nnUjxs5HrRFyPKstUIBTz4Dtceitj8&usqp=CAE', '256GB');
 
 -- --------------------------------------------------------
 
@@ -85,98 +62,22 @@ INSERT INTO `product` (`id`, `productname`, `price`, `quantity`, `catagory`, `im
 -- Table structure for table `soldproduct`
 --
 
-CREATE TABLE `soldproduct` (
-  `id` int(11) NOT NULL,
-  `productid` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `price` varchar(128) NOT NULL,
-  `address` int(11) NOT NULL,
-  `zipcode` varchar(128) NOT NULL,
-  `delivery` varchar(128) NOT NULL DEFAULT 'no',
-  `Orderdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE soldproduct (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    productid INT NOT NULL,
+    userid INT NOT NULL,
+    quantity INT NOT NULL,
+    price VARCHAR(128) NOT NULL,
+    address VARCHAR(20),
+    zipcode VARCHAR(128) NOT NULL,
+    Orderdate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (productid) REFERENCES product(id),
+    FOREIGN KEY (userid) REFERENCES user(id)
+);
 
---
--- Dumping data for table `soldproduct`
---
 
-INSERT INTO `soldproduct` (`id`, `productid`, `userid`, `quantity`, `price`, `address`, `zipcode`, `delivery`, `Orderdate`) VALUES
-(5, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(6, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(7, 3, 30, 100, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(8, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(9, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(10, 3, 30, 100, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(11, 7, 30, 1, '10', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(12, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(13, 3, 30, 10, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(14, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(15, 7, 30, 1, '10', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(16, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(17, 7, 30, 1, '10', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(18, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(19, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(20, 7, 30, 1, '10', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(21, 3, 30, 100, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(22, 9, 30, 1, '200', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(23, 3, 30, 100, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(24, 9, 30, 1, '200', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(25, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(26, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(27, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(28, 7, 30, 1, '10', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(29, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(30, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(31, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(32, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(33, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(34, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(35, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(36, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(37, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(38, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(39, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(40, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(41, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(42, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(43, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(44, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(45, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(46, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(47, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(48, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(49, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(50, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(51, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(52, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(53, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(54, 2, 30, 1, '3', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(55, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(56, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(57, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(58, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(59, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(60, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(61, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(62, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(63, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(64, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(65, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(66, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(67, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(68, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(69, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(70, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(71, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(72, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(73, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(74, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(75, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(76, 2, 30, 1, '3', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(77, 2, 30, 1, '3', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(78, 2, 30, 1, '3', 0, '1229', 'no', '2018-03-18 04:14:08'),
-(79, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:15:20');
+
+
 
 -- --------------------------------------------------------
 
@@ -184,84 +85,28 @@ INSERT INTO `soldproduct` (`id`, `productid`, `userid`, `quantity`, `price`, `ad
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `username` varchar(128) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `phone` varchar(16) NOT NULL,
-  `address` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    username VARCHAR(128) NOT NULL UNIQUE,
+    email VARCHAR(128) NOT NULL UNIQUE,
+    password VARCHAR(128) NOT NULL,
+    phone VARCHAR(16) NOT NULL,
+    address VARCHAR(256) NOT NULL,
+    wallet_bal INT
+);
+
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `username`, `email`, `password`, `phone`, `address`) VALUES
-(30, 'Md. Hasan Uzzaman', 'noyon', 'noyon892@gmail.com', 'sha1$57f1ff4a$1$ba87cb8290a3bafe3d283d25f4a33d22e681cb2f', '1733844422', 'Mahajon Bari,Affratpara,Chatmohar,Pabna'),
-(31, 'Md. Hasan Uzzaman', 'noyon892', 'noyon@gmail.com', 'sha1$3ea3ed64$1$43ed3d8b7e5f8c07163bc5e0c8c7d1005c77de2e', '01733844422', 'Mahajon Bari,Affratpara,Chatmohar,Pabna'),
-(32, 'Md. Hasan Uzzaman', 'noyon8920', 'noyon8920@gmail.com', 'sha1$56996c2c$1$a9abbf21a11ce77b4952a2e18d00bdbd27d3e605', '01733844422', 'Mahajon Bari,Affratpara,Chatmohar,Pabna'),
-(33, 'Souvik', 'souvik420', 'Souvik420@gmail.com', 'sha1$066daa5e$1$d341b8bf72a59ce10a256fc5d92a584a6a74ace4', '01768864040', 'Kahalu,Bogura'),
-(34, 'Md. Hasan Uzzaman', 'jame', 'jame@gmail.com', 'sha1$381f4664$1$81c2590f85491f6fb0275dfc3a065fc1b85e4ab7', '01733844422', 'Mahajon Bari,Affratpara,Chatmohar,Pabna');
+INSERT INTO user (name, username, email, password, phone, address, wallet_bal) 
+VALUES ('Samuel E George', 'samuel', 'samuel@gmail.com', 'sha1$6786d27c$1$ab2b210a0871658cba4f618224b1258ef4dcfe7d', '917470120073', 'jhalwa', 15140);
 
---
--- Indexes for dumped tables
---
+INSERT INTO user (name, username, email, password, phone, address, wallet_bal) 
+VALUES ('Shashank Shashidhar', 'sensei', 'shashank@gmail.com', 'sha1$4fc34040$1$3e4c59ed172f1c01268a188d86c71605b2f32b1e', '917975250289', 'jhalwa', 19964);
 
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `username` (`username`);
+INSERT INTO user (name, username, email, password, phone, address, wallet_bal) 
+VALUES ('saketh', 'saketh', 'saketh@gmail.com', 'sha1$6f4de423$1$e9de4986cce82f82954eb1ceaa18eb324a25e07e', '911234567890', 'jhalwa', NULL);
 
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `soldproduct`
---
-ALTER TABLE `soldproduct`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT for table `soldproduct`
---
-ALTER TABLE `soldproduct`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
